718cb3c8edd3b6e4619825d621b0e73519742338
zolob <zolobs90@gmail.com>
2026-01-27 22:31:26 +0200

    implemented book repository, controller, service

--- src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
@@ -1,5 +1,43 @@
 package com.epam.rd.autocode.spring.project.controller;
 
+import com.epam.rd.autocode.spring.project.dto.BookDTO;
+import com.epam.rd.autocode.spring.project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
-    // TODO Place your code here
+
+    private final BookService bookService;
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/book/{name}")
+    public ResponseEntity<BookDTO> getBookByName(@PathVariable String name){
+        return ResponseEntity.ok(bookService.getBookByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> addBook(@RequestBody BookDTO bookDTO){
+        return new ResponseEntity<>(bookService.addBook(bookDTO), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{name}")
+    public ResponseEntity<BookDTO> updateBook(@PathVariable String name, @RequestBody BookDTO bookDTO){
+        return ResponseEntity.ok(bookService.updateBookByName(name, bookDTO));
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Void> deleteBook(@PathVariable String name){
+        bookService.deleteBookByName(name);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/epam/rd/autocode/spring/project/service/impl/BookServiceImpl.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/impl/BookServiceImpl.java
@@ -1,5 +1,71 @@
 package com.epam.rd.autocode.spring.project.service.impl;
 
-public class BookServiceImpl{
-    //TODO Place your code here
+import com.epam.rd.autocode.spring.project.dto.BookDTO;
+import com.epam.rd.autocode.spring.project.exception.AlreadyExistException;
+import com.epam.rd.autocode.spring.project.exception.NotFoundException;
+import com.epam.rd.autocode.spring.project.model.Book;
+import com.epam.rd.autocode.spring.project.repo.BookRepository;
+import com.epam.rd.autocode.spring.project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(book -> modelMapper.map(book, BookDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public BookDTO getBookByName(String name) {
+        Book book = bookRepository.findByName(name)
+                .orElseThrow(() -> new NotFoundException("Book not found with name: " + name));
+        return modelMapper.map(book, BookDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public BookDTO addBook(BookDTO bookDTO) {
+        if (bookRepository.findByName(bookDTO.getName()).isPresent()){
+            throw new AlreadyExistException("Book already exists with name: " + bookDTO.getName());
+        }
+        Book book = modelMapper.map(bookDTO, Book.class);
+        Book saveBook = bookRepository.save(book);
+        return modelMapper.map(saveBook, BookDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public BookDTO updateBookByName(String name, BookDTO bookDTO) {
+        Book existingBook = bookRepository.findByName(name)
+                .orElseThrow(() -> new NotFoundException("Book not found with name: " + name));
+
+        Book newBookData = modelMapper.map(bookDTO, Book.class);
+        newBookData.setId(existingBook.getId());
+
+        Book updatedBook = bookRepository.save(newBookData);
+        return modelMapper.map(updatedBook, BookDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public void deleteBookByName(String name) {
+        Book book = bookRepository.findByName(name)
+                .orElseThrow(() -> new NotFoundException("Book not found with name: " + name));
+        bookRepository.deleteByName(name);
+    }
+
+
 }
