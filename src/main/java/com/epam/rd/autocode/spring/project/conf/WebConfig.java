3d9c9c285acda6a65821f439ee04dc851354845f
zolob <zolobs90@gmail.com>
2026-02-04 13:19:36 +0200

    Ukrainian and English localization added

---
+++ src/main/java/com/epam/rd/autocode/spring/project/conf/WebConfig.java
@@ -0,0 +1,46 @@
+package com.epam.rd.autocode.spring.project.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("uk"));
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms =
+                new ReloadableResourceBundleMessageSource();
+        ms.setBasename("classpath:i18n/messages");
+        ms.setDefaultEncoding("UTF-8");
+        ms.setFallbackToSystemLocale(false);
+        return ms;
+    }
+}
--- src/main/java/com/epam/rd/autocode/spring/project/model/enums/OrderStatus.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/enums/OrderStatus.java
@@ -1,8 +1,18 @@
 package com.epam.rd.autocode.spring.project.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderStatus {
-    NEW,
-    CONFIRMED,
-    COMPLETED,
-    CANCELED
+    NEW("order.status.NEW"),
+    COMPLETED("order.status.COMPLETED"),
+    CANCELED("order.status.CANCELED"),
+    CONFIRMED("order.status.CONFIRMED");
+
+    private final String key;
+
+    OrderStatus(String key) {
+        this.key = key;
+    }
+
 }
